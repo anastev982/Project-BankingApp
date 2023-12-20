9e6bacfe2bb7402cce9ce19e62d0614d96956e8b
Ana Stevanovic <149584536+anastev982@users.noreply.github.com>
2023-12-20 13:35:40 +0100

    Add files via upload

--- project_Banking_App.py
+++ project_Banking_App.py
@@ -12,47 +12,50 @@ class Bank:
       self.balance -= amount
       
     else:
-      self.balance < amount
       print("You dont have enough money on your account")
-      
+       
 def start():
+
     account_number = int(input("Please enter your account number"))
     initial_balance = int(input("Enter your balance: "))
+    
     bank = Bank(account_number, initial_balance)
     
     while True:
-      print("Deposit")
-      print("Withdrow")
-      print("Balance")
-      print("Exit")
+      print("1.Deposit")
+      print("2.Withdrawal")
+      print("3.Balance")
+      print("4.Exit")
     
       selection = input("Please select one of the options:")
     
-      if selection == "Deposit":
-        amount = int(input("Please enter the amount you will deposit: "))
-        bank.deposit(amount)
+      if selection == "1":
+           amount = int(input("Please enter the amount you will deposit: "))
+           bank.deposit(amount)
+           
+           if amount in range(10, 100000):
+            print("Money have been placed on your account")   
+           else:
+            print("Error!Please try again")
+            
+            continue
         
-        if amount in range(10, 100000):
-          print("Money have been placed on your account")
-        else:
-          print("Error")  
-      
-          
-      elif selection == "Withdrow":
-        amount = int(input("What is amount to withdrow?"))
-        bank.withdrow(amount)
+      elif selection == "2":
+          amount = int(input("What is amount you would like to withdraw?"))
+          bank.withdrawa(amount)
         
-        if amount in range(10, 1000):
-          print("You can take your money")
-        else:
-          print("You do not have enoph money on your account")  
-        
-      
-      elif selection == "Balance":
-        print(f"Your balance is: {bank.balance}")
+          if amount in range(10, 1000):
+              print("You can take your money")
+          else:
+              print("You do not have enough money on your account")  
         
       
-      elif selection == "Exit":
-        break
+      elif selection == "3":
+       print(f"Your balance is: {bank.balance}")
+      
+      elif selection == "4":
+         print("Thank you for using our bank") 
+         
+         break
     
 start()         
